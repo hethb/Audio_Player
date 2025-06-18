13afeeebfbb252ff2a0a0f8124efbe1b46b5354e
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-06-18 15:59:13 -0700

    menu bar to quit

--- StudyApp/src/defaul/Runner.java
+++ StudyApp/src/defaul/Runner.java
@@ -8,7 +8,12 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Runner extends JFrame implements MouseListener, ActionListener, KeyListener{
@@ -45,8 +50,60 @@ public class Runner extends JFrame implements MouseListener, ActionListener, Key
 	       frame.setUndecorated(false);
 	       frame.setSize(width, height);
 	       frame.add(cards);
-	       
+	       addMenus();
 	       frame.setVisible(true);
+	}
+	
+	public void addMenus() {
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem menuItem;
+		
+		//Create the menu bar.
+		menuBar = new JMenuBar();
+		//Build the first menu.
+		menu = new JMenu("Menu");
+		menu.setMnemonic(KeyEvent.VK_A);
+		menu.getAccessibleContext().setAccessibleDescription(
+		        "The only menu in this program that has menu items");
+		menuBar.add(menu);
+		menuItem = new JMenuItem("Quit", new ImageIcon("images/middle.gif"));
+		menuItem.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Define an array of custom options for the dialog
+		        Object[] options = { "Yes", "Cancel" };
+		        // Display an option dialog with custom options
+		        // The user's choice is stored in the 'choice'
+		        // variable
+		        int choice = JOptionPane.showOptionDialog(
+		            null, // Parent component (null means center on screen)
+		            "Do you want to proceed?", // Message to display
+		            "Quit the Game", // Dialog title
+		            JOptionPane.YES_NO_CANCEL_OPTION, // Option type (Yes, No, Cancel)
+		            JOptionPane.QUESTION_MESSAGE, // Message type (question icon)
+		            null, // Custom icon (null means no custom icon)
+		            options, // Custom options array
+		            options[1] // Initial selection (default is "Cancel")
+		        );
+		        // Check the user's choice and display a
+		        // corresponding message
+		        if (choice == JOptionPane.YES_OPTION) {
+		            // If the user chose 'Yes'
+		            // show a message indicating that they are
+		            // proceeding
+		            System.exit(0);
+		        }
+		        else {
+		            // If the user chose 'Cancel' or closed the
+		            // dialog
+		            // show a message indicating the operation is
+		            // canceled
+		            JOptionPane.showMessageDialog(null, "Operation canceled.");
+		        }
+			}	
+		});
+		menu.add(menuItem);
+		frame.setJMenuBar(menuBar);
 	}
 
 	@Override
