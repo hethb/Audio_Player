8b6a7e1782ce1b0310c2f4538fd27e4b85a87005
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-06-18 15:50:04 -0700

    setting up the open screen

--- StudyApp/src/defaul/OpenScreen.java
+++ StudyApp/src/defaul/OpenScreen.java
@@ -1,4 +1,15 @@
 package defaul;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public class OpenScreen extends StartScreen {
 	
@@ -12,6 +23,30 @@ public class OpenScreen extends StartScreen {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	 @Override
+	    protected void paintComponent(Graphics g) {
+	        super.paintComponent(g);
+	        
+	        Graphics2D g2d = (Graphics2D) g;
+	       
+
+	        g.setColor(Color.white);
+	        g.fillRect(620, 475, 380, 120); // box dimensions
+
+	        g.setColor(Color.black);
+	        Font theFont = new Font("Arial", Font.BOLD, 20);
+	        g.setFont(theFont);
+
+	        FontMetrics fm = g.getFontMetrics();
+	        int lineHeight = fm.getHeight();
+	        int maxWidth = 360; // Max width for text inside the box (380 - 20 padding)
+
+	        int x = 630;
+	        int y = 500;
+
+	        
+	    }
 
 	@Override
 	public void onHide() {
--- StudyApp/src/defaul/Runner.java
+++ StudyApp/src/defaul/Runner.java
@@ -1,5 +1,6 @@
 package defaul;
 
+import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -8,12 +9,17 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Runner extends JFrame implements MouseListener, ActionListener, KeyListener{
 	
 	JFrame frame;
+	private CardLayout cardLayout;
+	private JPanel cards; // The container for the screens
 	private StartScreen startScreen;
 	private OpenScreen openScreen;
+	private int width 	= 1020;
+	private int height 	= 900;
 	
 	
 	
@@ -28,7 +34,19 @@ public class Runner extends JFrame implements MouseListener, ActionListener, Key
 	public Runner() {
 		
 		
+		frame = new JFrame("Endless Game");
+	       cardLayout = new CardLayout();
+	       cards = new JPanel(cardLayout);
+		
 		openScreen = new OpenScreen();
+		
+		cards.add(openScreen, "Open Screen");
+	       // Setup the JFrame
+	       frame.setUndecorated(false);
+	       frame.setSize(width, height);
+	       frame.add(cards);
+	       
+	       frame.setVisible(true);
 	}
 
 	@Override
@@ -52,7 +70,7 @@ public class Runner extends JFrame implements MouseListener, ActionListener, Key
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		repaint();
 	}
 
 	@Override
